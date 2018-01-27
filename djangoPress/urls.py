c5947e3b7b43dc086c882a6ba972789487aa71bb
Yoan Ballesteros <b.yoan@live.fr>
2018-01-27 20:57:52 +0100

    Translate some URLs

--- djangoPress/urls.py
+++ djangoPress/urls.py
@@ -17,24 +17,25 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
-from app.views import IndexView, ArticleDetails, ArticleCreation, RegisterView, \
-    LoginView, ArticleUpdate, ArticleDelete, ProfileView
+from app.views import IndexView, ArticleDetails, ArticleCreation, RegisterView,\
+    \
+    LoginView, ArticleUpdate, ArticleDelete, ProfileView, SearchView
 from django.contrib.auth import views as auth_views
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'create$', ArticleCreation.as_view(), name="createArticle"),
-    url(r'update/(?P<pk>\d+)', ArticleUpdate.as_view(), name="updateArticle"),
-    url(r'delete/(?P<pk>\d+)', ArticleDelete.as_view(), name="deleteArticle"),
-    url(r'profile/(?P<pk>\w+)', ProfileView.as_view(), name='profile'),
-    url(r'(?P<pk>\d+)', ArticleDetails.as_view(), name="article"),
     url(r'login', LoginView.as_view(), name="login"),
     url(r'register', RegisterView.as_view(), name="register"),
     url(r'logout', auth_views.logout, {'next_page': '/'}, name='logout')
 ]
 
 urlpatterns += i18n_patterns(
+    url(_(r'profile/(?P<pk>\w+)'), ProfileView.as_view(), name='profile'),
+    url(_(r'create$'), ArticleCreation.as_view(), name="createArticle"),
+    url(_(r'update/(?P<pk>\d+)'), ArticleUpdate.as_view(), name="updateArticle"),
+    url(_(r'delete/(?P<pk>\d+)'), ArticleDelete.as_view(), name="deleteArticle"),
     url(_(r'^$'), IndexView.as_view(), name="index"),
+    url(_(r'(?P<pk>\d+)'), ArticleDetails.as_view(), name="article"),
 )
